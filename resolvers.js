ef866fe879a6b47dc0a9a935613f37a282101273
Bilal Ahmed Janjua <bilalahmjan@gmail.com>
2020-04-04 22:02:23 +0500

    Package updated with new data on some queries.

--- resolvers.js
+++ resolvers.js
@@ -1,40 +1,79 @@
 const axios = require("axios").default;
-const covid = require("novelcovid");
+const { NovelCovid } = require("novelcovid");
 
+const covid = new NovelCovid();
+
+const NOVEL_V1_API = "https://corona.lmao.ninja";
 const NOVEL_V2_API = "https://corona.lmao.ninja/v2";
 
 const resolvers = {
   Query: {
-    all: async () => await covid.getAll(),
-    countries: async (parent, { sort }, context, info) =>
-      sort ? await covid.getCountry({ sort }) : await covid.getCountry(),
+    all: async () => await covid.all(),
+    yesterday: async (parent, { sort }, context, info) => {
+      const countries = await (await axios.get(`${NOVEL_V1_API}/yesterday`))
+        .data;
+      if (!countries) return null;
+      return sort
+        ? countries.sort((a, b) => parseFloat(b[sort]) - parseFloat(a[sort]))
+        : countries;
+    },
+    countries: async (parent, { sort }, context, info) => {
+      const countries = await covid.countries();
+      return sort
+        ? countries.sort((a, b) => parseFloat(b[sort]) - parseFloat(a[sort]))
+        : countries;
+    },
     country: async (parent, { name: country }, context, info) =>
-      await covid.getCountry({ country }),
-    states: async (parent, { sort }, context, info) =>
-      sort ? await covid.getState({ sort }) : await covid.getState(),
-    state: async (parent, { name: state }, context, info) =>
-      await covid.getState({ state }),
-    historical: async () =>
-      await (await axios.get(`${NOVEL_V2_API}/historical`)).data,
-    historicalByCountry: async (parent, { name }, context, info) =>
-      await (await axios.get(`${NOVEL_V2_API}/historical/${name}`)).data,
-    jhucsse: async () => await (await axios.get(`${NOVEL_V2_API}/jhucsse`)).data,
+      await covid.countries(country),
+    states: async (parent, { sort }, context, info) => {
+      const states = await covid.states();
+      return sort
+        ? states.sort((a, b) => parseFloat(b[sort]) - parseFloat(a[sort]))
+        : states;
+    },
+    state: async (parent, { name: state }, context, info) => {
+      const states = await covid.states();
+      return states.find(
+        res => res.state.toLowerCase() === state.toLowerCase()
+      );
+    },
+    historical: async () => await covid.historical(),
+    historicalByCountry: async (parent, { name: country }, context, info) =>
+      await covid.historical(null, country),
+    historicalByCountryProvince: async (
+      parent,
+      { country, province },
+      context,
+      info
+    ) =>
+      await (
+        await axios.get(`${NOVEL_V2_API}/historical/${country}/${province}`)
+      ).data,
+    jhucsse: async () => await covid.jhucsse(),
     worldwideHistoricalData: async () => {
-      const data = await (await axios.get(`${NOVEL_V2_API}/historical`)).data
-      const dates = Object.keys(data[0].timeline.cases);
-      const cases = dates.map(date => {
-        return {
+      const data = await covid.historical(true);
+      const dates = Object.keys(data.cases);
+
+      const cases = [];
+      const deaths = [];
+      const recovered = [];
+
+      dates.forEach(date => {
+        cases.push({
           date,
-          count: data.reduce((sum, current) => sum + current.timeline.cases[date], 0) 
-        }
-      });
-      const deaths = dates.map(date => {
-        return {
+          count: data.cases[date]
+        });
+        deaths.push({
           date,
-          count: data.reduce((sum, current) => sum + current.timeline.deaths[date], 0) 
-        }
+          count: data.deaths[date]
+        });
+        recovered.push({
+          date,
+          count: data.recovered[date]
+        });
       });
-      return { cases, deaths };
+
+      return { cases, deaths, recovered };
     }
   }
 };
